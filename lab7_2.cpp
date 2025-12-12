a41e2028da8a6067fd77050e1d73fa6d651ee1d5
sirichat <sirichatcnx60@gmail.com>
2025-12-12 16:48:37 +0700

    last vesion

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,43 +1,48 @@
-//Dialog of the first example is given below.
 #include<iostream>
-using namespace std;
 #include<string>
+using namespace std;
 
 int main(){
     string name;
     cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
     cout << "?????: ";
-    getline(cin,name);
-    cout << "Fahsai: Wow!!! " << name <<  " is a really cool name.\n";
+    getline(cin, name);
+
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
     cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+
     string ID;
     cout << name << ": ";
     cin >> ID;
+
     int gear = ((ID[0])-48)*10 +(ID[1])-48;
-    cout << "Fahsai: I think you may be GEAR " << gear-12 << ". I have a free movie ticket for you.\n";
+    cout << "Fahsai: I think you may be GEAR " << gear-12 
+         << ". I have a free movie ticket for you.\n";
+
     cout << "Fahsai: Let's go to the cinema together!!!\n";
     cout << "Fahsai: What movie do you want to watch?\n";
+
     string movie;
     cout << name << ": ";
-    getline(cin,movie);
+    cin.ignore();          
+    getline(cin, movie);
+
     cout << "Fahsai: So....which day are you free to go with me?\n";
+
     string days;
     cout << name << ": ";
-    getline(cin,days);
-    cout << "Fahsai: "<< days << "....that is OK!!! I'm looking forward to watching " << movie << "with you .\n";
+    getline(cin, days);
+
+    cout << "Fahsai: " << days 
+         << "....that is OK!!! I'm looking forward to watching " 
+         << movie << " with you.\n";
+
     string last_mess;
     cout << name << ": ";
-    cin >> last_mess;
-    cout << "Fahsai: 555+ see you "<< days << ". Bye Bye \\(^ ^)/\n";
+    getline(cin, last_mess);
 
+    cout << "Fahsai: 555+ see you " << days 
+         << ". Bye Bye \\(^ ^)/\n";
 
-
-
-    
-
-
-
-    
-
+    return 0;
 }
-
